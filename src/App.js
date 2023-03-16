60d64b110276ad8bce353dd9adfc406daa22e2a1
Barnabas5035 <110441604+Barnabas5035@users.noreply.github.com>
2023-03-15 18:51:51 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -6,60 +6,60 @@ import MasterPage from './Component/mainPage/MasterPage'
 const App = () => {
   return (
     <MasterPage>
-      <main className='mt-5 pt-2'>
+<main className='mt-5 pt-2'>
         <div className='container-fluid'>
           <div className='row'>
             <div className='col-md-12 fw-bold fs-3 mt-5 '>Dashboard</div>
           </div>
           <div className='row'>
             <div className='col-md-3 mb-3 '>
-              <div class='card text-white bg-primary  h-90'>
-                <div class='card-body'>
-                  <h5 class='card-title'>20%</h5>
-                  <h5 class='card-title'>Neworder</h5>
-                  <div class='card-header bg-primary mt-3'>
+              <div className='card text-white bg-primary  h-90'>
+                <div className='card-body'>
+                  <h5 className='card-title'>20%</h5>
+                  <h5 className='card-title'>Neworder</h5>
+                  <div className='card-header bg-primary mt-3'>
                     More info
                     <span>
-                      <i class='bi bi-chevron-down'></i>
+                      <i className='bi bi-chevron-down'></i>
                     </span>
                   </div>
                 </div>
               </div>
             </div>
             <div className='col-md-3 mb-3'>
-              <div class='card text-white bg-warning  h-90'>
-                <div class='card-body'>
-                  <h5 class='card-title'>67%</h5>
-                  <h5 class='card-title'>Bounce Rate</h5>
-                  <div class='card-header'>
+              <div className='card text-white bg-warning  h-90'>
+                <div className='card-body'>
+                  <h5 className='card-title'>67%</h5>
+                  <h5 className='card-title'>Bounce Rate</h5>
+                  <div className='card-header'>
                     More info
                     <span>
-                      <i class='bi bi-chevron-down'></i>
+                      <i className='bi bi-chevron-down'></i>
                     </span>
                   </div>
                 </div>
               </div>
             </div>
             <div className='col-md-3 mb-3 '>
-              <div class='card text-white bg-danger  h-90'>
-                <div class='card-body'>
-                  <h5 class='card-title'>15</h5>
-                  <h5 class='card-title'>Registration</h5>
-                  <div class='card-header'>
+              <div className='card text-white bg-danger  h-90'>
+                <div className='card-body'>
+                  <h5 className='card-title'>15</h5>
+                  <h5 className='card-title'>Registration</h5>
+                  <div className='card-header'>
                     More info
                     <span>
-                      <i class='bi bi-chevron-down'></i>
+                      <i className='bi bi-chevron-down'></i>
                     </span>
                   </div>
                 </div>
               </div>
             </div>
             <div className='col-md-3 mb-3 '>
-              <div class='card text-white bg-success h-90 '>
-                <div class='card-body'>
-                  <h5 class='card-title'>150</h5>
-                  <h5 class='card-title'>visitor</h5>
-                  <div class='card-header'>
+              <div className='card text-white bg-success h-90 '>
+                <div className='card-body'>
+                  <h5 className='card-title'>150</h5>
+                  <h5 className='card-title'>visitor</h5>
+                  <div className='card-header'>
                     More info
                     <span>
                       <i class='bi bi-chevron-down'></i>
